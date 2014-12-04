78d45bc80e53985f96e98f04158b5eb52db170b9
Dennis Crissman <dcrissma@redhat.com>
2014-12-04 13:04:57 -0500

    change daemon from simply dying upon failure, but rather to attempt restart

--- consistency-checker/src/main/java/com/redhat/lightblue/migrator/consistency/ConsistencyCheckerDaemon.java
+++ consistency-checker/src/main/java/com/redhat/lightblue/migrator/consistency/ConsistencyCheckerDaemon.java
@@ -10,61 +10,115 @@ import org.apache.commons.daemon.support.DaemonLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ConsistencyCheckerDaemon implements Daemon{
+public final class ConsistencyCheckerDaemon implements Daemon{
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ConsistencyCheckerDaemon.class);
 
-    private Thread consistencyCheckerThread;
-    private Timer isThreadAliveWatcher;
+    private DaemonContext context;
+    private Thread consistencyCheckerThread = null;
+
+    private Timer isThreadAliveWatcher = null;
 
     public static void main(final String[] args) throws Exception {
-        DaemonLoader.load(ConsistencyCheckerDaemon.class.getCanonicalName(), args);
+        DaemonLoader.load(ConsistencyCheckerDaemon.class.getName(), args);
         DaemonLoader.start();
     }
 
     @Override
     public void init(DaemonContext context) throws DaemonInitException, Exception {
-        ConsistencyChecker checker = ConsistencyCheckerCLI.buildConsistencyChecker(context.getArguments());
-        consistencyCheckerThread = new Thread(checker, "ConsistencyCheckerRunner");
-        isThreadAliveWatcher = new Timer("ConsistencyCheckerStatusMonitor");
+        LOGGER.info("Initializing " + getClass().getSimpleName());
+
+        this.context = context;
+
+        consistencyCheckerThread = createConsistencyCheckerThread();
     }
 
     @Override
-    public void start() throws Exception {
-        LOGGER.info("Starting " + getClass().getName());
+    public synchronized void start() throws Exception {
+        LOGGER.info("Starting " + getClass().getSimpleName());
+
         consistencyCheckerThread.start();
-        
-        /*
-         * Timer insures that if the underlying ConsistencyChecker stopped for any reason,
-         * that the service will be shutdown also. This really shouldn't happen,
-         * so if it does, it is considered an error.
-         */
-        isThreadAliveWatcher.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                if(!consistencyCheckerThread.isInterrupted() && !consistencyCheckerThread.isAlive()){
-                    LOGGER.error(ConsistencyChecker.class.getName() + " has stopped running, killing service.");
-                    DaemonLoader.stop();
-                    DaemonLoader.destroy();
-                    System.exit(1);
-                }
-            }
-        }, 5000);
+
+        if(isThreadAliveWatcher == null){
+            isThreadAliveWatcher = new Timer("WorkerMonitor");
+            isThreadAliveWatcher.scheduleAtFixedRate(new IsAliveTimerTask(-1), 5000, 5000);
+        }
     }
 
     @Override
-    public void stop() throws Exception {
-        LOGGER.info("Stopping " + getClass().getName());
+    public synchronized void stop() throws Exception {
+        LOGGER.info("Stopping " + getClass().getSimpleName());
+
         isThreadAliveWatcher.cancel();
-        if((consistencyCheckerThread != null) && consistencyCheckerThread.isAlive()){
+
+        if(consistencyCheckerThread != null){
             consistencyCheckerThread.interrupt();
         }
     }
 
     @Override
     public void destroy() {
+        LOGGER.info("Destroying " + getClass().getSimpleName());
+
         consistencyCheckerThread = null;
         isThreadAliveWatcher = null;
+    }
+
+    private Thread createConsistencyCheckerThread(){
+        return new Thread(
+                ConsistencyCheckerCLI.buildConsistencyChecker(context.getArguments()),
+                "ConsistencyCheckerRunner");
+    }
+
+    /**
+     * Ensures that if the ConsistencyChecker Tread goes down for any reason
+     * that the Daemon handles it appropriately by either attempting a restart
+     * or shutting down.
+     */
+    private class IsAliveTimerTask extends TimerTask{
+
+        private final int maxRetries;
+        private int retriesAttempted = 0;
+
+        public IsAliveTimerTask(int maxRetries){
+            this.maxRetries = maxRetries;
+        }
+
+        @Override
+        public synchronized void run() {
+            if(!consistencyCheckerThread.isInterrupted()
+                    && !consistencyCheckerThread.isAlive()){
+
+                if(maxRetries < 0){
+                    retry();
+                }
+                else if(retriesAttempted >= maxRetries){
+                    shutdownDaemon();
+                }
+                else{
+                    retry();
+                    retriesAttempted ++;
+                }
+            }
+            else{
+                retriesAttempted = 0;
+            }
+        }
+
+        private void retry(){
+            LOGGER.warn(ConsistencyChecker.class.getSimpleName() + " has stopped running, attempting restart.");
+            consistencyCheckerThread = createConsistencyCheckerThread();
+            System.gc();
+            DaemonLoader.start();
+        }
+
+        private void shutdownDaemon(){
+            LOGGER.error(ConsistencyChecker.class.getSimpleName() + " has stopped running, killing service.");
+            DaemonLoader.stop();
+            DaemonLoader.destroy();
+            System.exit(1);
+        }
+
     }
 
 }
