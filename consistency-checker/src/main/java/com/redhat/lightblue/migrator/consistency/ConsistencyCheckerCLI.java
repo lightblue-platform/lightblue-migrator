611df95c1d88401b12bd657fc9bddcb602e64d36
Dennis Crissman <dcrissma@redhat.com>
2015-03-09 14:03:35 -0400

    clean up logging more

--- consistency-checker/src/main/java/com/redhat/lightblue/migrator/consistency/ConsistencyCheckerCLI.java
+++ consistency-checker/src/main/java/com/redhat/lightblue/migrator/consistency/ConsistencyCheckerCLI.java
@@ -12,7 +12,7 @@ import org.slf4j.LoggerFactory;
 
 public final class ConsistencyCheckerCLI {
 
-    public static final Logger LOGGER = LoggerFactory.getLogger(ConsistencyCheckerCLI.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConsistencyCheckerCLI.class);
 
     private ConsistencyCheckerCLI() {
 
