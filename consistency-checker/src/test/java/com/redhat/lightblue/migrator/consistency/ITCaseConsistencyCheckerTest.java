5e51e43b70d08d23b146bccbcd13b6424e6ee92c
Dennis Crissman <dcrissma@redhat.com>
2015-04-14 16:25:53 -0400

    write some sample migrator integration tests

--- consistency-checker/src/test/java/com/redhat/lightblue/migrator/consistency/ITCaseConsistencyCheckerTest.java
+++ consistency-checker/src/test/java/com/redhat/lightblue/migrator/consistency/ITCaseConsistencyCheckerTest.java
@@ -1,9 +1,12 @@
 package com.redhat.lightblue.migrator.consistency;
 
 import static com.redhat.lightblue.util.test.AbstractJsonNodeTest.loadJsonNode;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 import java.util.Iterator;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -13,11 +16,21 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class ITCaseConsistencyCheckerTest extends AbstractMigratorController {
 
+    private final ConsistencyChecker consistencyChecker;
+    private static boolean initialized = false;
+
     public ITCaseConsistencyCheckerTest() throws Exception {
         super();
 
-        loadData("migrationConfiguration", "1.0.0", "./test/data/load-migration-configurations.json");
-        loadData("migrationJob", "1.0.0", "./test/data/load-migration-jobs.json");
+        if (!initialized) {
+            loadData("migrationConfiguration", "1.0.0", "./test/data/load-migration-configurations.json");
+            loadData("migrationJob", "1.0.0", "./test/data/load-migration-jobs.json");
+            initialized = true;
+        }
+
+        consistencyChecker = new ConsistencyChecker();
+        consistencyChecker.setClient(getLightblueClient());
+        consistencyChecker.setMigrationJobEntityVersion(parseEntityVersion((ObjectNode) loadJsonNode("./migrationJob.json")));
     }
 
     @Override
@@ -45,9 +58,29 @@ public class ITCaseConsistencyCheckerTest extends AbstractMigratorController {
         return node;
     }
 
+    private String parseEntityVersion(ObjectNode node) {
+        ObjectNode schema = (ObjectNode) node.get("schema");
+        ObjectNode version = (ObjectNode) schema.get("version");
+        return version.get("value").textValue();
+    }
+
     @Test
-    public void test() {
-        assertTrue(true);
+    public void testGetMigrationJobs() {
+        MigrationConfiguration config = new MigrationConfiguration();
+        config.setConfigurationName("customers");
+        List<MigrationJob> jobs = consistencyChecker.getMigrationJobs(config);
+
+        assertNotNull(jobs);
+        assertEquals(1, jobs.size());
+    }
+
+    /**
+     * Assert that null is returned when there is not a next available job.
+     */
+    @Test
+    public void testGetNextAvailableJob() {
+        MigrationJob job = consistencyChecker.getNextAvailableJob();
+        assertNull(job);
     }
 
 }
