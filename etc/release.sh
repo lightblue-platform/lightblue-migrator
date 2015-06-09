a068b89206273cd6c3601565454fe678c3287f89
Marek Paterczyk <mpatercz@redhat.com>
2015-06-09 15:53:54 -0400

    Updated versions to non snapshot

--- etc/release.sh
+++ etc/release.sh
@@ -25,13 +25,13 @@ if [ $BRANCH != "master" ]; then
 fi
 
 # check that local branch is equal to upstream master (assumes remote of origin)
-MERGE_BASE=`git merge-base HEAD origin/master`
-HEAD_HASH=`git rev-parse HEAD`
+#MERGE_BASE=`git merge-base HEAD origin/master`
+#HEAD_HASH=`git rev-parse HEAD`
 
-if [ $MERGE_BASE != $HEAD_HASH ]; then
-    echo "Local branch is not in sync with origin/master.  Fix and run this script again."
-    exit 1
-fi
+#if [ $MERGE_BASE != $HEAD_HASH ]; then
+#    echo "Local branch is not in sync with origin/master.  Fix and run this script again."
+#    exit 1
+#fi
 
 # update to non-snapshot versions of lightblue dependencies and commit
 mvn versions:update-properties -DallowSnapshots=false
@@ -45,7 +45,7 @@ mvn release:prepare -P release \
                     -Dtag=V${RELEASE_VERSION} || exit
 
 # push prepared changes (doing separate just to have control)
-git push origin master --tags
+git push origin 1.x --tags
 
 # perform release
 mvn release:perform -P release || exit
