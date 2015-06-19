46b45e4054341f0284e6e4cf72cf9d13dc3436db
Burak Serdar <bserdar@redhat.com>
2015-06-19 08:54:59 -0600

    Run tests with forks

--- migrator/src/test/java/com/redhat/lightblue/migrator/ControllerTest.java
+++ migrator/src/test/java/com/redhat/lightblue/migrator/ControllerTest.java
@@ -25,14 +25,14 @@ import static com.redhat.lightblue.util.test.AbstractJsonNodeTest.loadJsonNode;
 import static com.redhat.lightblue.client.expression.query.ValueQuery.withValue;
 import static com.redhat.lightblue.client.projection.FieldProjection.includeFieldRecursively;
 
-public class MigratorTest extends AbstractMigratorController {
+public class ControllerTest extends AbstractMigratorController {
 
     private String versionMigrationJob;
     private String versionMigrationConfiguration;
     private String versionSourceCustomer;
     private String versionDestinationCustomer;
 
-    public MigratorTest() throws Exception {}
+    public ControllerTest() throws Exception {}
         
     @Override
     protected JsonNode[] getMetadataJsonNodes() throws Exception {
@@ -69,67 +69,66 @@ public class MigratorTest extends AbstractMigratorController {
         cli.data(req);
     }
 
-
     @Test
-    public void migrateTest() throws Exception {
+    public void controllerTest() throws Exception {
         clearData();
-        loadData("migrationConfiguration", versionMigrationConfiguration, "./test/data/load-migration-configurations.json");
+        loadData("migrationConfiguration", versionMigrationConfiguration, "./test/data/load-migration-configurations-testmigrator.json");
         loadData("migrationJob", versionMigrationJob, "./test/data/load-migration-jobs.json");
-        loadData("sourceCustomer", versionSourceCustomer, "./test/data/load-source-customers.json");
-       
+        
         MainConfiguration cfg=new MainConfiguration();
         cfg.setName("continuum");
         cfg.setHostName("hostname");
         Controller controller=new Controller(cfg);
 
-        // Stop when we retrieve source docs
-        Breakpoint.stop("Migrator:sourceDocs");
+        Breakpoint.stop("Controller:start");
+        Breakpoint.stop("Controller:loadconfig");
+        Breakpoint.stop("Controller:createconfig");
+        
         controller.start();
 
-        Breakpoint.waitUntil("Migrator:sourceDocs");
-        // We got source docs, peek
-        Thread[] threads=new Thread[1];
-        Assert.assertEquals(1,controller.
-                            getMigrationProcesses().
-                            get("customerMigration_0").mig.getMigratorThreads().enumerate(threads));
+        Breakpoint.waitUntil("Controller:start");
+        System.out.println("Resuming controller after startup");
+        Breakpoint.resume("Controller:start");
+
+        Breakpoint.waitUntil("Controller:loadconfig");
+        System.out.println("Controller will load configurations");
+        Breakpoint.resume("Controller:loadconfig");
+
+        // Put breakpoints in migrator controller before we start one
+        Breakpoint.stop("MigratorController:start");
+        Breakpoint.stop("MigratorController:findandlock");
+        Breakpoint.stop("MigratorController:process");
+        Breakpoint.stop("MigratorController:unlock");
+        Breakpoint.stop("MigratorController:end");
         
-        Migrator m=(Migrator)threads[0];
-        Assert.assertEquals(5,m.getSourceDocs().size());
+        Breakpoint.waitUntil("Controller:createconfig");
+        System.out.println("Checking controllers");
 
-        Breakpoint.stop("Migrator:destDocs");
-        Breakpoint.resume("Migrator:sourceDocs");
-
-        Breakpoint.waitUntil("Migrator:destDocs");
-        Assert.assertEquals(0,m.getDestDocs().size());
-                
-        Breakpoint.stop("Migrator:insertDocs");
-        Breakpoint.resume("Migrator:destDocs");
-
-        Breakpoint.waitUntil("Migrator:insertDocs");
-        Assert.assertEquals(5,m.getInsertDocs().size());
-
-        Breakpoint.stop("Migrator:rewriteDocs");
-        Breakpoint.resume("Migrator:insertDocs");
-
-        Breakpoint.waitUntil("Migrator:rewriteDocs");
-        Assert.assertEquals(0,m.getRewriteDocs().size());
-
-        Breakpoint.stop("Migrator:complete");
-        Breakpoint.resume("Migrator:rewriteDocs");
-
-        Breakpoint.waitUntil("Migrator:complete");
-
-        LightblueClient cli = new LightblueHttpClient();
-        DataFindRequest req=new DataFindRequest("destCustomer","1.0.0");
-        req.select(new FieldProjection("*",true,true));
-        req.where(new ValueQuery("objectType",ExpressionOperation.EQ,"destCustomer"));
-        req.sort(new SortCondition("_id",SortDirection.ASC));
-        JsonNode[] ret=cli.data(req,JsonNode[].class);
+        Map<String,Controller.MigrationProcess> prc=controller.getMigrationProcesses();
+        Assert.assertEquals(1,prc.size());
         
-        Breakpoint.resume("Migrator:complete");
+        Breakpoint.resume("Controller:createconfig");
 
-        Assert.assertEquals(5,ret.length);
+        // Controller created threads, now check the migrator controller thread progress
+
+        Breakpoint.waitUntil("MigratorController:start");
+        Breakpoint.resume("MigratorController:start");
+        System.out.println("Migrator controller started");
+
+        Breakpoint.waitUntil("MigratorController:findandlock");
+        Breakpoint.resume("MigratorController:findandlock");
+
+        Breakpoint.waitUntil("MigratorController:process");
+        System.out.println("Processing");
+        TestMigrator.count=0;
+        Breakpoint.resume("MigratorController:process");
+
+        Breakpoint.waitUntil("MigratorController:unlock");
+        // At this point, there must be one TestMigrator instance running
+        Assert.assertEquals(1,TestMigrator.count);
+        Breakpoint.resume("MigratorController:unlock");
+
+        controller.interrupt();
     }
-         
 }
 
