af842c0b7879e5dd78262de5457b50af15af9dbb
Naveen Malik <nmalik@redhat.com>
2015-10-21 14:20:47 -0400

    Updated TogglzRandomUsername to generate 1000001 user names instead of 101

--- facade/src/main/java/com/redhat/lightblue/migrator/features/TogglzRandomUsername.java
+++ facade/src/main/java/com/redhat/lightblue/migrator/features/TogglzRandomUsername.java
@@ -6,18 +6,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Generates random username for togglz gradual activation strategy and stores it in ThreadLocal. Threads are
- * pooled and reused, so you need to call init method each time a decision is to be made where to send the
- * request (source or destination/lightblue). Beginning of a public DAO method is a good place.
+ * Generates random username for togglz gradual activation strategy and stores
+ * it in ThreadLocal. Threads are pooled and reused, so you need to call init
+ * method each time a decision is to be made where to send the request (source
+ * or destination/lightblue). Beginning of a public DAO method is a good place.
  *
  * @author mpatercz
  *
  */
 public abstract class TogglzRandomUsername {
 
-    private static ThreadLocal<String> threadLocalUsername = new ThreadLocal<String>();
+    private static final ThreadLocal<String> threadLocalUsername = new ThreadLocal<>();
 
-    private static Random random = new Random();
+    private static final Random random = new Random();
 
     public static final Logger log = LoggerFactory.getLogger(TogglzRandomUsername.class);
 
@@ -25,8 +26,10 @@ public abstract class TogglzRandomUsername {
         // Togglz is calculating a hash value from the username which is then normalized to a value between 0 and 100
         // Considering we're starting from 0-100 value, this is an unnecessary overhead. A price we need to pay for using Togglz
         // to do load balancing...
-        String username = new Integer(random.nextInt(100)).toString();
-        log.debug("Generated username="+username);
+        String username = Integer.toString(random.nextInt(1000000));
+        if (log.isDebugEnabled()) {
+            log.debug("Generated username=" + username);
+        }
         threadLocalUsername.set(username);
     }
 
