163dfe892b69147de775d0c4399db942fa1ed411
Naveen Malik <nmalik@redhat.com>
2015-11-18 11:24:02 -0500

    Updated to use latest 2.0 client, which has breaking changes

--- migrator/src/test/java/com/redhat/lightblue/migrator/FailTest.java
+++ migrator/src/test/java/com/redhat/lightblue/migrator/FailTest.java
@@ -4,26 +4,21 @@ import java.util.Map;
 
 import org.junit.Test;
 import org.junit.Assert;
-import org.junit.Before;
-import org.junit.After;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.redhat.lightblue.client.LightblueClient;
+import com.redhat.lightblue.client.Projection;
+import com.redhat.lightblue.client.Query;
+import com.redhat.lightblue.client.http.LightblueHttpClient;
+import com.redhat.lightblue.client.request.data.DataDeleteRequest;
+import com.redhat.lightblue.client.request.data.DataFindRequest;
+import com.redhat.lightblue.client.response.LightblueResponse;
 
-import com.redhat.lightblue.client.*;
-import com.redhat.lightblue.client.request.*;
-import com.redhat.lightblue.client.response.*;
-import com.redhat.lightblue.client.http.*;
-import com.redhat.lightblue.client.request.data.*;
-import com.redhat.lightblue.client.expression.query.*;
-import com.redhat.lightblue.client.enums.*;
-import com.redhat.lightblue.client.projection.*;
 
 
 import static com.redhat.lightblue.util.test.AbstractJsonNodeTest.loadJsonNode;
 
-import static com.redhat.lightblue.client.expression.query.ValueQuery.withValue;
-import static com.redhat.lightblue.client.projection.FieldProjection.includeFieldRecursively;
 
 public class FailTest extends AbstractMigratorController {
 
@@ -59,13 +54,13 @@ public class FailTest extends AbstractMigratorController {
     public void clearData() throws Exception {
         LightblueClient cli = new LightblueHttpClient();
         DataDeleteRequest req=new DataDeleteRequest("activeExecution",null);
-        req.where(new ValueQuery("objectType",ExpressionOperation.EQ,"activeExecution"));
+        req.where(Query.withValue("objectType", Query.BinOp.eq, "activeExecution"));
         cli.data(req);
         req=new DataDeleteRequest("migrationJob",null);
-        req.where(new ValueQuery("objectType",ExpressionOperation.EQ,"migrationJob"));
+        req.where(Query.withValue("objectType", Query.BinOp.eq, "migrationJob"));
         cli.data(req);
         req=new DataDeleteRequest("migrationConfiguration",null);
-        req.where(new ValueQuery("objectType",ExpressionOperation.EQ,"migrationConfiguration"));
+        req.where(Query.withValue("objectType", Query.BinOp.eq, "migrationConfiguration"));
         cli.data(req);
     }
 
@@ -95,8 +90,8 @@ public class FailTest extends AbstractMigratorController {
         // Here, it should have failed already
         LightblueClient cli=new LightblueHttpClient();
         DataFindRequest req=new DataFindRequest("migrationJob",null);
-        req.where(withValue("objectType = migrationJob"));
-        req.select(includeFieldRecursively("jobExecutions.0.errorMsg"));
+        req.where(Query.withValue("objectType = migrationJob"));
+        req.select(Projection.includeFieldRecursively("jobExecutions.0.errorMsg"));
         LightblueResponse resp=cli.data(req);
         JsonNode node=resp.getJson();
         System.out.println("Response:"+node);
